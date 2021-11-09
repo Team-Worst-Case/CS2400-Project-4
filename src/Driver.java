ebf4b87675d6bc6fc0efde5ed0151bfba3230b93
Roberto Tabares <robctabs08@gmail.com>
2021-11-08 19:09:45 -0800

    adding and editing readIntegers method

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,28 @@
+
+import java.util.Scanner;
+
+
+
 public class Driver
 {
 	public static void main(String[] args)
 	{
 		System.out.println("test");
+
+
+		public static Int[] readIntegers(String dataFileName){
+			
+			File dataFile = new File();
+			Scanner scnr = new Scanner(dataFile);
+
+			while(scnr.hasNextInt())
+			{
+				readInt = scnr.nextInt();
+
+				i++;
+			}
+
+			scnr.close();
+		}
 	}
 }
