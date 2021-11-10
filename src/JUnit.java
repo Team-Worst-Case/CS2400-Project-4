d02b7a31e4fca845d730634e1e46dcc9e0bff124
Roberto Tabares <robctabs08@gmail.com>
2021-11-10 12:28:06 -0800

    added removeMax and add methods for tests

--- src/JUnit.java
+++ src/JUnit.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 
 
 
-
 public class  JUnit
 {
     public void testOptimalMaxHeapMethod {
@@ -13,16 +12,44 @@ public class  JUnit
         MaxHeap<Integer> optimalHeap = new MaxHeap<>();
         optimalHeap.MaxHeap(testValues);
 
+        Integer[] testResult = new Integer[5];
+
         for (int index = 0; index < optimalHeap.getSize(); index++) {
 
-            result[index-1] = optimalHeap.getMax(index);
+            testResult[index-1] = optimalHeap.getMax(index);
         }
 
-        expected = {50, 20, 80, 40, 10, 30};
+        Integer[] expectedResult = {50, 20, 80, 40, 10, 30};
+
+        assertArrayEquals(expectedResult, testResult);
     }
 
 
     public void testSequentialMethod {
+        // Almost the same thing as optimal method, just need to change a couple things
+        /*
+        Integer[] testValues = {10,20,30,40,50,80};
+        MaxHeap<Integer> optimalHeap = new MaxHeap<>();
+        optimalHeap.MaxHeap(testValues);
+
+        Integer[] testResult = new Integer[5];
+
+        for (int index = 0; index < optimalHeap.getSize(); index++) {
+
+            testResult[index-1] = optimalHeap.getMax(index);
+        }
+
+        Integer[] expectedResult = {50, 20, 80, 40, 10, 30};
+
+        assertArrayEquals(expectedResult, testResult);
+        */
+    }
+
+    public void testRemoveMaxMethod {
+
+    }
+
+    public void testAddMethod {
 
     }
 }
