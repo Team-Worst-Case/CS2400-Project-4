49fe9279adb17b0d1f281a38b104ec285e5c32fb
Roberto Tabares <robctabs08@gmail.com>
2021-11-12 21:51:59 -0800

    added newTests methods

--- src/JUnit.java
+++ src/JUnit.java
@@ -98,4 +98,40 @@ public class  JUnit
 
         assertArrayEquals(expectedResult, testResult);
     }
+
+    public void newTestSequentialMethod() {
+        Integer[] grabFromThis = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16} ;
+        MaxHeap<Integer> expected = new MaxHeap<>();
+
+        for (int i=0; i<grabFromThis.length; ++i) 
+        {
+            expected.add(grabFromThis[i]);
+        }
+
+        MaxHeap<Integer> testSequen = new MaxHeap<>();
+
+        testSequen.add(grabFromThis);
+        assertEquals(expected.getMax(), testSequen.getMax());
+        
+        while (expected.getSize() > 0) {
+            assertEquals(expected.removeMax(), testSequen.removeMax());
+        }
+ 
+    }
+
+    @Test
+    public void newTestOptimalMethods() {
+        Integer[] testOptimal = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32};
+        MaxHeap<Integer> expected = new MaxHeap<>(testOptimal);
+        MaxHeap<Integer> optiHeap = new MaxHeap<>();
+        optiHeap.MaxHeap(testOptimal);
+
+        assertEquals(expected.getMax(), optiHeap.getMax());
+
+        while (expected.getSize() > 0) {
+            assertEquals(expected.removeMax(), optiHeap.removeMax());
+        }
+ 
+    }
 }
+    
