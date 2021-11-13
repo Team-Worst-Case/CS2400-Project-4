007860a16c22c9d5cefe6e7b7a630d7c1ede233e
Christopher <programmerchristopher@gmail.com>
2021-11-12 22:12:07 -0800

    JUnit debug

--- src/JUnit.java
+++ src/JUnit.java
@@ -22,7 +22,7 @@ public class  JUnit
 
 	@Test
 	public void test3() {
-		MaxHeap<Integer> testHeap = new MaxHeap<>();
+		MaxHeap testHeap = new MaxHeap<>();
 		testHeap.add(2);
 		testHeap.add(4);
 		testHeap.add(6);
@@ -99,6 +99,7 @@ public class  JUnit
         assertArrayEquals(expectedResult, testResult);
     }
 
+    @Test
     public void newTestSequentialMethod() {
         Integer[] grabFromThis = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16} ;
         MaxHeap<Integer> expected = new MaxHeap<>();
@@ -110,7 +111,6 @@ public class  JUnit
 
         MaxHeap<Integer> testSequen = new MaxHeap<>();
 
-        testSequen.add(grabFromThis);
         assertEquals(expected.getMax(), testSequen.getMax());
         
         while (expected.getSize() > 0) {
@@ -124,7 +124,6 @@ public class  JUnit
         Integer[] testOptimal = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32};
         MaxHeap<Integer> expected = new MaxHeap<>(testOptimal);
         MaxHeap<Integer> optiHeap = new MaxHeap<>();
-        optiHeap.MaxHeap(testOptimal);
 
         assertEquals(expected.getMax(), optiHeap.getMax());
 
